5a7ee0726e368cb1a5cb16ffe6042859b713a915
3numeret3 <47710573+mantua381@users.noreply.github.com>
2019-03-02 01:44:33 +1100

    Add files via upload
    CraxkerZ Script Plate 2019
    "Love it or love it mode" 
    Usage example: 
    
    CZ 1 +
    
    Usage example 2:
    
    Sp00fm4k on
    Sp00fip on
    monitormode on #or mm +, or 3 on etc... got it ?
    
    %%% Add yee own scripts option
    
    Haxck th3 world cr4ck tha planet
    
    %%% help develop this sxript plate
    %%% join our fckn t3am :)
    
    Hall of fame:
    
    Yer contributerz CraxkerZ Script Plate -2019 vs. Unique-ng2019a team cheerup

---
+++ motherboard-2019.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#CrackerZ 2019 Script Plate
+
+
+echo 'CrackerZ .ž 2019 Temper Script Plate ng++'
+
+echo 'be your mostly W3lc0me'
+
+echo 'please turn on / off yer scripts: '
+
+echo '--[type script name and on or offs'
+
+echo 'example, type:'
+echo 'sp00fm4k +      or type: 1 +'
+echo 'or rihgt from tha cli, anytime type: CZ sp00f_m4k off     or CZ sp00fip +    for examples.'
+
+echo 'thank ye for being our h4ppy customer. and...'
+
+echo '"may the Force be with yae"'
+echo ''
+echo '00 or ctrl c to exit'
+echo ' %%%%%%%%'
+echo '1 sp00fm4k (+,-)'
+echo '2 sp00fip (+,-)'
+echo '3 monitormode (+,-)'
+echo '%%%%%%%%'
+echo 'add script type add file.sh or script.py or haxkingengine.js, suported: js, c, c++, pp, lua, py, bash'
